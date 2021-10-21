c64e037fb08a9aa8a36dfeb4027cdc371b658b82
thisisaarush <tanwaraarush007@gmail.com>
2021-10-21 12:20:59 +0530

    initial commit

---
+++ server/src/services/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://nasa-api:<password>@nasacluster.otot6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+}
